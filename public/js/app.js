5c8b1faabc29da6a4839ec0ae18a2648ec998698
0xPurp <haroune1020@hotmail.com>
2021-06-19 16:36:21 +0200

    push final : finitialision de tout le site html sass js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-import {mesIndicateurs, images} from "./carousel.js"
+import {indicator,img} from "./carousel.js"
 import {emporium} from "./main.js"
 import {modeJour, modeNuit} from "./modenuit.js" 
 import {inscrit,enregistre} from "./modal.js" 
